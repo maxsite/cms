69a1a11ef89132ca6ca004e2c41981b87a4e8837
cuprum <cuprum.name@gmail.com>
2014-08-22 13:57:01 +0400

    remove word "Обновить" (#11)

--- application/maxsite/admin/plugins/admin_page/all-files.php
+++ application/maxsite/admin/plugins/admin_page/all-files.php
@@ -42,7 +42,7 @@ if ($n = mso_segment(3)) // указан N номер записи
 	}
 	
 	
-	$all_files = '<div class="all-files-nav"><a href="' . getinfo('site_admin_url') . 'files/' . $current_dir . '" target="_blank" class="goto-files">' . t('Управление файлами') . '</a> <a href="#" id="all-files-update" class="all-files-update">' . t('Обновить') . '</a></div>';
+	$all_files = '<div class="all-files-nav"><a href="' . getinfo('site_admin_url') . 'files/' . $current_dir . '" target="_blank" class="goto-files">' . t('Управление файлами') . '</a></div>';
 	
 	// скрипт выполняет аякс
 	// первый раз при загрузке страницы

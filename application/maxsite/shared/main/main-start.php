98de59479a7036ce8fe46f57f73c5ed9c8d07e9a
MAX <maxsite.org@gmail.com>
2015-02-18 13:39:42 +0200

    Add to main-start.php page-out & thumb

--- application/maxsite/shared/main/main-start.php
+++ application/maxsite/shared/main/main-start.php
@@ -5,6 +5,12 @@
  * (c) http://max-3000.com/
  */
 
+# библиотека для вывода записей в цикле и вывод колонок
+require_once(getinfo('shared_dir') . 'stock/page-out/page-out.php');
+
+# библиотека для работы с изображениями
+require_once(getinfo('shared_dir') . 'stock/thumb/thumb.php');
+
 ob_start();
 
 # end file

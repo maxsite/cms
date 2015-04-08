d5fcdd6e4e35587c05ae807102fc19c70626285b
MAX <maxsite.org@gmail.com>
2015-04-08 12:24:16 +0300

    Полная переделка last_pages

--- application/maxsite/plugins/last_pages/info.php
+++ application/maxsite/plugins/last_pages/info.php
@@ -3,7 +3,7 @@
 $info = array(
 	'name' => t('Последние записи'),
 	'description' => t('Вывод последних записей'),
-	'version' => '1.6',
+	'version' => '2.0',
 	'author' => 'Максим',
 	'plugin_url' => 'http://max-3000.com/',
 	'author_url' => 'http://maxsite.org/',

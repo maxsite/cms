ddf7634366b9aaa2ab62df5fb4c30a17dc92555e
MAX <maxsite.org@gmail.com>
2015-04-24 20:20:49 +0300

    Правки в /type/page/units/

--- application/maxsite/shared/type/page/units/page-comments-article-tmpl.php
+++ application/maxsite/shared/type/page/units/page-comments-article-tmpl.php
@@ -1,32 +1,30 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed'); ?>
 
-<article class="{{ $a_class }} clearfix">
+<article class="comment-article {{ $a_class }} clearfix">
 	
-	<img src="{{ $avatar }}" alt="" class="gravatar right">
+	<img src="{{ $avatar }}" alt="" class="gravatar">
 	
 	<p class="comment-info">
-		<span class="comment-num">№ {{ $comment_num }}</span> | 
+		<span class="comment-num">{{ $comment_num }}</span>
 		
-		<span class="url">{{ $comments_url }}
+		<span class="comment-author">{{ $comments_url }}</span>
 		
 		{% if ($comusers_url) : %}
-			| <a href="{{ $comusers_url }}" rel="nofollow" class="comuser-url">{{ tf('Сайт') }}</a>
+			<a href="{{ $comusers_url }}" rel="nofollow" class="comuser-url">{{ tf('Сайт') }}</a>
 		{% endif %}
 		
-		</span>
-		
-		{% if ($edit_link) : %}
-			| <a href="{{ $edit_link . $comments_id }}" class="comment-edit">edit</a>
-		{% endif %}
+		<a href="#comment-{{ $comments_id }}" id="comment-{{ $comments_id }}" class="comment-date">{{ mso_date_convert('d-m-Y H:i', $comments_date) }}</a>
 		
 		{% if (!$comments_approved) : %}
-			| <span class="comment-approved">{{ tf('Ожидает модерации') }}</span>
+			<span class="comment-approved">{{ tf('Ожидает модерации') }}</span>
 		{% endif %}
 		
-		| <span class="comment-date"><a href="#comment-{{ $comments_id }}" id="comment-{{ $comments_id }}">{{ mso_date_convert('d-m-Y H:i', $comments_date) }}</a></span>
+		{% if ($edit_link) : %}
+			<a href="{{ $edit_link . $comments_id }}" class="comment-edit">edit</a>
+		{% endif %}
+		
 	</p>
 	
 	<div class="comment-content">{{ $comments_content }}</div>
 	
 </article>
-<hr>
--- application/maxsite/shared/type/page/units/page-comments-count-tmpl.php
+++ application/maxsite/shared/type/page/units/page-comments-count-tmpl.php
@@ -1,5 +1,5 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed'); ?>
 
 <p class="comments-count">
-	{{ tf('Комментариев') }}: {{ count($comments) }} | <span class="comments-rss"><a href="{{ mso_page_url($page['page_slug']) }}/feed">RSS</a></span>
+	<span class="comments-all-count">{{ tf('Комментариев') }}: {{ count($comments) }}</span> <a href="{{ mso_page_url($page['page_slug']) }}/feed" class="comments-rss">RSS</a>
 </p>

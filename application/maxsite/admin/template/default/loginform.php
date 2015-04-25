c64c0a8fa620b1690329d963f2c20fe30646afb6
MAX <maxsite.org@gmail.com>
2015-04-25 20:13:56 +0300

    Поправил верстку формы admin-логина

--- application/maxsite/admin/template/default/loginform.php
+++ application/maxsite/admin/template/default/loginform.php
@@ -10,14 +10,11 @@
 <meta charset="UTF-8">
 <meta name="generator" content="MaxSite CMS">
 <title><?= 'MaxSite CMS &ndash; ' . t('Вход в админ-панель') ?></title>
-<link rel="shortcut icon" href="<?= getinfo('template_url') . 'images/favicons/' . mso_get_option('default_favicon', 'templates', 'favicon1.png') ?>" type="image/x-icon">
 <link rel="stylesheet" href="<?=  getinfo('admin_url') . 'template/' . mso_get_option('admin_template', 'general', 'default') . '/loginform.css'; ?>">
 </head>
-
 <body>
 <div class="all">
 	<div class="all-wrap">
-
 		<div class="logo">
 			<a href="http://max-3000.com/" class="logo-cms" title="<?= t('MaxSite CMS — cистема управления сайтом') ?>"></a>
 			
@@ -35,21 +32,22 @@
 			$redirect_url = (isset($_SERVER['HTTP_REFERER'])) ? $_SERVER['HTTP_REFERER'] : getinfo('siteurl') . mso_current_url();
 			
 			mso_remove_hook('login_form_auth'); # удалим все хуки для авторизации
-					
-			mso_login_form(array( 
-					'login' => t('Логин'), 
-					'password' => t('Пароль'), 
-					'submit' => '', 
-					'submit_value' => t('Войти'),
-					'login_add' => ' placeholder="' . t('ваш логин') . '"',
-					'password_add' => ' placeholder="' . t('ваш пароль') . '"',
-				),
-				$redirect_url);
-		}
-		?>
+			
+
+?>
+<form method="post" action="<?= $MSO->config['site_url'] . 'login' ?>" name="flogin">
+	<input type="hidden" value="<?= $redirect_url ?>" name="flogin_redirect">
+	<input type="hidden" value="<?= $MSO->data['session']['session_id'] ?>" name="flogin_session_id">
+	
+	<p><label><?= t('Логин') ?><br><input type="text" value="" name="flogin_user" placeholder="<?= t('ваш логин') ?>"></label></p>
+	
+	<p><label><?= t('Пароль') ?><br><input type="password" value="" name="flogin_password" placeholder="<?= t('ваш пароль') ?>"></label></p>
+	
+	<p><button type="submit" name="flogin_submit"><?= t('Войти') ?></button></p>
+</form>
+<?php } ?>
 		
 		<div class="goto-site"><a href="<?= getinfo('siteurl') ?>"><?= t('Вернуться к сайту') ?></a></div>
-		<div class="clearfix"></div>
 	</div>
 </div>
 

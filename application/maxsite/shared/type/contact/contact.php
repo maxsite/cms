a2271729dfc4a55b1735868e265f48c58502daf6
MAX <maxsite.org@gmail.com>
2015-05-02 09:58:49 +0300

    Контактная форма

--- application/maxsite/shared/type/contact/contact.php
+++ application/maxsite/shared/type/contact/contact.php
@@ -24,31 +24,49 @@ echo mso_get_option('prew_contact', 'templates', '');
 if ($f = mso_page_foreach('contact-do')) require($f); // подключаем кастомный вывод
 
 $form_def = '[form]
-[subject=Пожелания по сайту # Нашел ошибку на сайте # Подскажите, пожалуйста]
+
+[options]
+email = ' . mso_get_option('admin_email', 'general', 'admin@site.com') . '
+[/options]
+
+[field] 
+require = 1
+type = select
+description = Тема письма
+values = Пожелания по сайту # Нашел ошибку на сайте # Подскажите, пожалуйста
+default = Пожелания по сайту 
+subject = 1
+[/field]
 
 [field]
-require = 0
+require = 1   
 type = text
-type_text = url
+description = Ваше имя
+placeholder = Ваше имя
+[/field]
+
+[field]
+require = 1   
+type = text
+clean = email
+description = Ваш email
+placeholder = Ваш email
+from = 1
+[/field]
+
+[field] 
+require = 0   
+type = url
 description = Сайт
 tip = Вы можете указать адрес своего сайта (если есть)
 placeholder = Адрес сайта
 [/field]
 
-[field]
-require = 0
-type = text
-description = Телефон
-tip = Телефон лучше указывать с кодом города/страны
-placeholder = Введите свой телефонный номер
-[/field]
-
-[field]
-require = 1
-type = textarea
+[field] 
+require = 1 
+type = textarea 
 description = Ваш вопрос
 placeholder = О чем вы хотите написать?
-attr = style="height: 200px"
 [/field]
 
 [/form]';
@@ -56,17 +74,17 @@ attr = style="height: 200px"
 $form_def = str_replace("\r", "", $form_def);
 $form_def = str_replace("\n", "_NR_", $form_def);
 
-
 $form = mso_get_option('form_contact', 'templates', $form_def);
 
-//pr($form);
+// pr($form);
+
+if (!$form) $form = $form_def;
 
 // используем плагин Forms
 if (!function_exists('forms_content'))
 {
 	require_once(getinfo('plugins_dir') . 'forms/index.php');
 }
-	
 	
 echo forms_content(str_replace("_NR_", "\n", $form));
 

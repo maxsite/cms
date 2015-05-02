5af99572e1d8fafb62fd1876e416e6f3cadcc09c
MAX <maxsite.org@gmail.com>
2015-05-02 22:06:05 +0300

    Правка tmpl форм

--- application/maxsite/shared/type/password-recovery/units/password-recovery-tmpl.php
+++ application/maxsite/shared/type/password-recovery/units/password-recovery-tmpl.php
@@ -4,16 +4,16 @@
 
 <p><a href="{{ getinfo('siteurl') }}users">{{ tf('Список комментаторов') }}</a></p>
 
-<form method="post">{{ mso_form_session('f_session_id') }}
+<div class="mso-recovery-form"><form method="post">{{ mso_form_session('f_session_id') }}
 
 	<p>{{ tf('Если у вас сохранился код активации, то вы можете сразу заполнить все поля. Если код активации утерян, то вначале введите только email и нажмите кнопку «Готово». На указанный email вы получите код активации. После этого вы можете вернуться на эту страницу и заполнить все поля.') }}</p>
 
-	<p><label>{{ tf('Ваш email') }} <input type="text" name="f_comusers_email" value=""></label></p>
+	<p><label><span>{{ tf('Ваш email') }}</span><input type="text" name="f_comusers_email" value=""></label></p>
 		
-	<p><label>{{ tf('Ваш код активации') }} <input type="text" name="f_comusers_activate_key" value=""></label></p>
+	<p><label><span>{{ tf('Ваш код активации') }}</span><input type="text" name="f_comusers_activate_key" value=""></label></p>
 
-	<p><label>{{ tf('Новый пароль') }} <input type="text" name="f_comusers_password" value=""></label></p>
+	<p><label><span>{{ tf('Новый пароль') }}</span><input type="text" name="f_comusers_password" value=""></label></p>
 
 	<p><button type="submit" name="f_submit">{{  tf('Готово') }}</button></p>
 
-</form>
+</form></div>
--- application/maxsite/shared/type/registration/units/registration-tmpl.php
+++ application/maxsite/shared/type/registration/units/registration-tmpl.php
@@ -1,6 +1,6 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed'); ?>
 
-<div class="loginform">
+<div class="mso-registration-form">
 	<h3>{{ tf('Укажите данные для регистрации на сайте') }}</h3>
 
 	<form method="post" action="{{ $action }}" name="freg">
@@ -8,15 +8,15 @@
 		<input type="hidden" value="0" name="freg_rules_ok">
 		<input type="hidden" value="{{ $redirect_url }}" name="freg_redirect_url">
 		
-		<p><label>{{ tf('Email (используется как логин)') }} <input type="email" name="freg_email" value="{{ $vreg_email }}"></label></p>
+		<p><label><span>{{ tf('Email (используется как логин)') }}</span><input type="email" name="freg_email" value="{{ $vreg_email }}"></label></p>
 		
-		<p><label>{{ tf('Пароль (английские буквы и цифры, без пробелов, минимум 6 символов)') }} <input type="password" name="freg_password" value="{{ $vreg_password }}"></label></p>
+		<p><label><span>{{ tf('Пароль (английские буквы и цифры, без пробелов, минимум 6 символов)') }}</span><input type="password" name="freg_password" value="{{ $vreg_password }}"></label></p>
 		
-		<p><label>{{ tf('Повторите пароль') }} <input type="password" name="freg_password_repeat" value="{{ $vreg_password_repeat }}"></label></p>		
+		<p><label><span>{{ tf('Повторите пароль') }}</span><input type="password" name="freg_password_repeat" value="{{ $vreg_password_repeat }}"></label></p>		
 		
-		<p><label>{{ tf('Имя') }} <input type="text" name="freg_nik" value="{{ $vreg_nik }}"></label></p>
+		<p><label><span>{{ tf('Имя') }}</span><input type="text" name="freg_nik" value="{{ $vreg_nik }}"></label></p>
 		
-		<p><label>{{ tf('Адрес сайта (если есть)') }} <input type="text" name="freg_url" value="{{ $vreg_url }}"></label></p>
+		<p><label><span>{{ tf('Адрес сайта (если есть)') }}</span><input type="text" name="freg_url" value="{{ $vreg_url }}"></label></p>
 		
 		<p><label><input type="checkbox" name="freg_rules_ok"> {{ tf('Обязуюсь соблюдать правила сайта') }} {{ $rules }}</label></p>
 		

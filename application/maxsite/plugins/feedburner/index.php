4481715ca882c3e4cd0351b407251a1ea8c0d2f1
MAX <maxsite.org@gmail.com>
2015-09-15 11:20:18 +0300

    Плагин feedburner 1.1

--- application/maxsite/plugins/feedburner/index.php
+++ application/maxsite/plugins/feedburner/index.php
@@ -8,14 +8,13 @@
 # функция автоподключения плагина
 function feedburner_autoload($args = array())
 {
-	mso_hook_add( 'admin_init', 'feedburner_admin_init'); # хук на админку
 	mso_hook_add( 'init', 'feedburner_init'); # хук на init
 }
 
 # функция выполняется при активации (вкл) плагина
 function feedburner_activate($args = array())
 {	
-	mso_create_allow('feedburner_edit', t('Админ-доступ к feedburner'));
+	mso_create_allow('feedburner_edit', t('Админ-доступ к опциям feedburner'));
 	return $args;
 }
 
@@ -26,64 +25,44 @@ function feedburner_uninstall($args = array())
 	return $args;
 }
 
-# функция выполняется при указаном хуке admin_init
-function feedburner_admin_init($args = array()) 
+# функция отрабатывающая миниопции плагина (function плагин_mso_options)
+function feedburner_mso_options() 
 {
 	if ( !mso_check_allow('feedburner_edit') ) 
 	{
-		return $args;
+		echo t('Доступ запрещен');
+		return;
 	}
 	
-	$this_plugin_url = 'plugin_feedburner'; // url и hook
-	
-	# добавляем свой пункт в меню админки
-	# первый параметр - группа в меню
-	# второй - это действие/адрес в url - http://сайт/admin/demo
-	#			можно использовать добавочный, например demo/edit = http://сайт/admin/demo/edit
-	# Третий - название ссылки	
-	
-	mso_admin_menu_add('plugins', $this_plugin_url, 'FeedBurner');
+	# ключ, тип, ключи массива
+	mso_admin_plugin_options('plugin_feedburner', 'plugins', 
+		array(
+			'key' => array(
+						'type' => 'text', 
+						'name' => t('Адрес вашего фида в FeedBurner.com:'), 
+						'description' => 'http://feeds.feedburner.com/<b>[адрес вашего фида]</b>',
+						'default' => ''
+					),
+			),
+		t('Плагин FeedBurner'), // титул
+		t('Плагин выполняет перенаправление вашего основного rss на сервис feedburner.com.')   // инфо
+	);
 
-	# прописываем для указаного admin_url_ + $this_plugin_url - (он будет в url) 
-	# связанную функцию именно она будет вызываться, когда 
-	# будет идти обращение по адресу http://сайт/admin/_null
-	mso_admin_url_hook ($this_plugin_url, 'feedburner_admin_page');
-	
-	return $args;
 }
-
-# функция вызываемая при хуке, указанном в mso_admin_url_hook
-function feedburner_admin_page($args = array()) 
-{
-	# выносим админские функции отдельно в файл
-	if ( !mso_check_allow('feedburner_admin_page') ) 
-	{
-		echo 'Доступ запрещен';
-		return $args;
-	}
-	
-	mso_hook_add_dinamic( 'mso_admin_header', ' return $args . "FeedBurner"; ' );
-	mso_hook_add_dinamic( 'admin_title', ' return "FeedBurner - " . $args; ' );
-	
-	require(getinfo('plugins_dir') . 'feedburner/admin.php');
-}
-
 
 # функции плагина
 function feedburner_init($args = array())
 {
-
 	if (!is_feed()) return $args;
 	
-	
 	$options = mso_get_option('plugin_feedburner', 'plugins', array());
-	if ( !isset($options['key']) ) return $args; 
+	if ( !isset($options['key']) && !$options['key'] ) return $args; 
 	
 	if ( !preg_match("!feedburner|feedvalidator!i", $_SERVER['HTTP_USER_AGENT']) ) // если это не feedburner, то делаем редирект на feedburner
 	{
 		if (mso_segment(1) == 'feed') // только для главной страницы
 		{
-			header("Location: http://feeds2.feedburner.com/" . trim($options['key']));
+			header("Location: http://feeds.feedburner.com/" . trim($options['key']));
 			header("HTTP/1.1 302 Temporary Redirect");
 			exit;
 		}
@@ -91,7 +70,5 @@ function feedburner_init($args = array())
 	
 	return $args;
 }
-
-
 
 # end file
--- application/maxsite/plugins/feedburner/info.php
+++ application/maxsite/plugins/feedburner/info.php
@@ -3,7 +3,7 @@
 $info = array(
 	'name' => t('RSS для FeedBurner.com'),
 	'description' => t('Подключение RSS блога для FeedBurner.com'),
-	'version' => '1.0',
+	'version' => '1.1',
 	'author' => 'Максим',
 	'plugin_url' => 'http://max-3000.com/',
 	'author_url' => 'http://maxsite.org/',

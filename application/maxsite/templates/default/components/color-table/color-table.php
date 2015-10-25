463bcdce269c11188bb43db00d7ca3908ba49b41
MAX <maxsite.org@gmail.com>
2015-10-25 13:08:56 +0200

    Новый компонент color-table подсказка по цветам шаблона

---
+++ application/maxsite/templates/default/components/color-table/color-table.php
@@ -0,0 +1,173 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+/*
+	(c) MaxSite CMS, http://max-3000.com/
+	
+	Вывод полоски всех цветов
+*/
+
+if (!is_login()) return;
+
+?>
+<div class="flex flex-wrap-tablet bg-white">
+<div class="flex-grow1 h20px bg-white bordered" title="white"></div>
+<div class="flex-grow1 h20px bg-gray" title="gray"></div>
+<div class="flex-grow1 h20px bg-black" title="black"></div>
+<div class="flex-grow1 h20px bg-red" title="red"></div>
+<div class="flex-grow1 h20px bg-green" title="green"></div>
+<div class="flex-grow1 h20px bg-blue" title="blue"></div>
+<div class="flex-grow1 h20px bg-yellow" title="yellow"></div>
+<div class="flex-grow1 h20px bg-orange" title="orange"></div>
+<div class="flex-grow1 h20px bg-olive" title="olive"></div>
+<div class="flex-grow1 h20px bg-teal" title="teal"></div>
+<div class="flex-grow1 h20px bg-purple" title="purple"></div>
+<div class="flex-grow1 h20px bg-pink" title="pink"></div>
+<div class="flex-grow1 h20px bg-brown" title="brown"></div>
+<div class="flex-grow1 h20px bg-color1" title="color1"></div>
+<div class="flex-grow1 h20px bg-colorA1" title="colorA1"></div>
+<div class="flex-grow1 h20px bg-color2" title="color2"></div>
+<div class="flex-grow1 h20px bg-colorA2" title="colorA2"></div>
+<div class="flex-grow1 h20px bg-color3" title="color3"></div>
+<div class="flex-grow1 h20px bg-colorA3" title="colorA3"></div>
+<div class="flex-grow1 h20px bg-color4" title="color4"></div>
+<div class="flex-grow1 h20px bg-colorA4" title="colorA4"></div>
+<div class="flex-grow1 h20px bg-color5" title="color5"></div>
+<div class="flex-grow1 h20px bg-colorA5" title="colorA5"></div>
+</div>
+
+<div class="flex flex-wrap-tablet bg-white">
+<div class="flex-grow1 h20px bg-red50" title="red50"></div>
+<div class="flex-grow1 h20px bg-red100" title="red100"></div>
+<div class="flex-grow1 h20px bg-red200" title="red200"></div>
+<div class="flex-grow1 h20px bg-red300" title="red300"></div>
+<div class="flex-grow1 h20px bg-red400" title="red400"></div>
+<div class="flex-grow1 h20px bg-red500" title="red, red500"></div>
+<div class="flex-grow1 h20px bg-red600" title="red600"></div>
+<div class="flex-grow1 h20px bg-red700" title="red700"></div>
+<div class="flex-grow1 h20px bg-red800" title="red800"></div>
+<div class="flex-grow1 h20px bg-red900" title="red900"></div>
+
+<div class="flex-grow1 h20px bg-green50" title="green50"></div>
+<div class="flex-grow1 h20px bg-green100" title="green100"></div>
+<div class="flex-grow1 h20px bg-green200" title="green200"></div>
+<div class="flex-grow1 h20px bg-green300" title="green300"></div>
+<div class="flex-grow1 h20px bg-green400" title="green400"></div>
+<div class="flex-grow1 h20px bg-green500" title="green, green500"></div>
+<div class="flex-grow1 h20px bg-green600" title="green600"></div>
+<div class="flex-grow1 h20px bg-green700" title="green700"></div>
+<div class="flex-grow1 h20px bg-green800" title="green800"></div>
+<div class="flex-grow1 h20px bg-green900" title="green900"></div>
+
+<div class="flex-grow1 h20px bg-blue50" title="blue50"></div>
+<div class="flex-grow1 h20px bg-blue100" title="blue100"></div>
+<div class="flex-grow1 h20px bg-blue200" title="blue200"></div>
+<div class="flex-grow1 h20px bg-blue300" title="blue300"></div>
+<div class="flex-grow1 h20px bg-blue400" title="blue400"></div>
+<div class="flex-grow1 h20px bg-blue500" title="blue, blue500"></div>
+<div class="flex-grow1 h20px bg-blue600" title="blue600"></div>
+<div class="flex-grow1 h20px bg-blue700" title="blue700"></div>
+<div class="flex-grow1 h20px bg-blue800" title="blue800"></div>
+<div class="flex-grow1 h20px bg-blue900" title="blue900"></div>
+
+<div class="flex-grow1 h20px bg-yellow50" title="yellow50"></div>
+<div class="flex-grow1 h20px bg-yellow100" title="yellow100"></div>
+<div class="flex-grow1 h20px bg-yellow200" title="yellow200"></div>
+<div class="flex-grow1 h20px bg-yellow300" title="yellow300"></div>
+<div class="flex-grow1 h20px bg-yellow400" title="yellow400"></div>
+<div class="flex-grow1 h20px bg-yellow500" title="yellow, yellow500"></div>
+<div class="flex-grow1 h20px bg-yellow600" title="yellow600"></div>
+<div class="flex-grow1 h20px bg-yellow700" title="yellow700"></div>
+<div class="flex-grow1 h20px bg-yellow800" title="yellow800"></div>
+<div class="flex-grow1 h20px bg-yellow900" title="yellow900"></div>
+</div>
+
+<div class="flex flex-wrap-tablet bg-white">
+<div class="flex-grow1 h20px bg-orange50" title="orange50"></div>
+<div class="flex-grow1 h20px bg-orange100" title="orange100"></div>
+<div class="flex-grow1 h20px bg-orange200" title="orange200"></div>
+<div class="flex-grow1 h20px bg-orange300" title="orange300"></div>
+<div class="flex-grow1 h20px bg-orange400" title="orange400"></div>
+<div class="flex-grow1 h20px bg-orange500" title="orange, orange500"></div>
+<div class="flex-grow1 h20px bg-orange600" title="orange600"></div>
+<div class="flex-grow1 h20px bg-orange700" title="orange700"></div>
+<div class="flex-grow1 h20px bg-orange800" title="orange800"></div>
+<div class="flex-grow1 h20px bg-orange900" title="orange900"></div>
+
+<div class="flex-grow1 h20px bg-olive50" title="olive50"></div>
+<div class="flex-grow1 h20px bg-olive100" title="olive100"></div>
+<div class="flex-grow1 h20px bg-olive200" title="olive200"></div>
+<div class="flex-grow1 h20px bg-olive300" title="olive300"></div>
+<div class="flex-grow1 h20px bg-olive400" title="olive400"></div>
+<div class="flex-grow1 h20px bg-olive500" title="olive, olive500"></div>
+<div class="flex-grow1 h20px bg-olive600" title="olive600"></div>
+<div class="flex-grow1 h20px bg-olive700" title="olive700"></div>
+<div class="flex-grow1 h20px bg-olive800" title="olive800"></div>
+<div class="flex-grow1 h20px bg-olive900" title="olive900"></div>
+
+<div class="flex-grow1 h20px bg-teal50" title="teal50"></div>
+<div class="flex-grow1 h20px bg-teal100" title="teal100"></div>
+<div class="flex-grow1 h20px bg-teal200" title="teal200"></div>
+<div class="flex-grow1 h20px bg-teal300" title="teal300"></div>
+<div class="flex-grow1 h20px bg-teal400" title="teal400"></div>
+<div class="flex-grow1 h20px bg-teal500" title="teal, teal500"></div>
+<div class="flex-grow1 h20px bg-teal600" title="teal600"></div>
+<div class="flex-grow1 h20px bg-teal700" title="teal700"></div>
+<div class="flex-grow1 h20px bg-teal800" title="teal800"></div>
+<div class="flex-grow1 h20px bg-teal900" title="teal900"></div>
+
+<div class="flex-grow1 h20px bg-violet50" title="violet50"></div>
+<div class="flex-grow1 h20px bg-violet100" title="violet100"></div>
+<div class="flex-grow1 h20px bg-violet200" title="violet200"></div>
+<div class="flex-grow1 h20px bg-violet300" title="violet300"></div>
+<div class="flex-grow1 h20px bg-violet400" title="violet400"></div>
+<div class="flex-grow1 h20px bg-violet500" title="violet, violet500"></div>
+<div class="flex-grow1 h20px bg-violet600" title="violet600"></div>
+<div class="flex-grow1 h20px bg-violet700" title="violet700"></div>
+<div class="flex-grow1 h20px bg-violet800" title="violet800"></div>
+<div class="flex-grow1 h20px bg-violet900" title="violet900"></div>
+</div>
+
+<div class="flex flex-wrap-tablet bg-white">
+<div class="flex-grow1 h20px bg-purple50" title="purple50"></div>
+<div class="flex-grow1 h20px bg-purple100" title="purple100"></div>
+<div class="flex-grow1 h20px bg-purple200" title="purple200"></div>
+<div class="flex-grow1 h20px bg-purple300" title="purple300"></div>
+<div class="flex-grow1 h20px bg-purple400" title="purple400"></div>
+<div class="flex-grow1 h20px bg-purple500" title="purple, purple500"></div>
+<div class="flex-grow1 h20px bg-purple600" title="purple600"></div>
+<div class="flex-grow1 h20px bg-purple700" title="purple700"></div>
+<div class="flex-grow1 h20px bg-purple800" title="purple800"></div>
+<div class="flex-grow1 h20px bg-purple900" title="purple900"></div>
+
+<div class="flex-grow1 h20px bg-pink50" title="pink50"></div>
+<div class="flex-grow1 h20px bg-pink100" title="pink100"></div>
+<div class="flex-grow1 h20px bg-pink200" title="pink200"></div>
+<div class="flex-grow1 h20px bg-pink300" title="pink300"></div>
+<div class="flex-grow1 h20px bg-pink400" title="pink400"></div>
+<div class="flex-grow1 h20px bg-pink500" title="pink, pink500"></div>
+<div class="flex-grow1 h20px bg-pink600" title="pink600"></div>
+<div class="flex-grow1 h20px bg-pink700" title="pink700"></div>
+<div class="flex-grow1 h20px bg-pink800" title="pink800"></div>
+<div class="flex-grow1 h20px bg-pink900" title="pink900"></div>
+
+<div class="flex-grow1 h20px bg-brown50" title="brown50"></div>
+<div class="flex-grow1 h20px bg-brown100" title="brown100"></div>
+<div class="flex-grow1 h20px bg-brown200" title="brown200"></div>
+<div class="flex-grow1 h20px bg-brown300" title="brown300"></div>
+<div class="flex-grow1 h20px bg-brown400" title="brown400"></div>
+<div class="flex-grow1 h20px bg-brown500" title="brown, brown500"></div>
+<div class="flex-grow1 h20px bg-brown600" title="brown600"></div>
+<div class="flex-grow1 h20px bg-brown700" title="brown700"></div>
+<div class="flex-grow1 h20px bg-brown800" title="brown800"></div>
+<div class="flex-grow1 h20px bg-brown900" title="brown900"></div>
+
+<div class="flex-grow1 h20px bg-gray50"  title="gray50"></div>
+<div class="flex-grow1 h20px bg-gray100" title="gray100"></div>
+<div class="flex-grow1 h20px bg-gray200" title="gray200"></div>
+<div class="flex-grow1 h20px bg-gray300" title="gray300"></div>
+<div class="flex-grow1 h20px bg-gray400" title="gray400"></div>
+<div class="flex-grow1 h20px bg-gray500" title="gray, gray500"></div>
+<div class="flex-grow1 h20px bg-gray600" title="gray600"></div>
+<div class="flex-grow1 h20px bg-gray700" title="gray700"></div>
+<div class="flex-grow1 h20px bg-gray800" title="gray800"></div>
+<div class="flex-grow1 h20px bg-gray900" title="gray900"></div>
+</div>

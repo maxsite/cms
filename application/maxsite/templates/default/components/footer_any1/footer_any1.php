d5c33bc50734aabd06edb214e8c9ed47fefc6d7b
MAX <maxsite.org@gmail.com>
2015-10-25 13:08:21 +0200

    Новый компонент для подвала footer_any1

---
+++ application/maxsite/templates/default/components/footer_any1/footer_any1.php
@@ -0,0 +1,30 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed'); 
+/*
+    (c) MaxSite CMS, http://max-3000.com/
+*/
+
+// условие вывода компонента
+// php-условие как в виджетах
+if ($rules = trim(mso_get_option('footer_any1_rules_output', getinfo('template'), '')))
+{
+	$rules_result = eval('return ( ' . $rules . ' ) ? 1 : 0;');
+	if ($rules_result === false) $rules_result = 1;
+	if ($rules_result !== 1) return;
+}
+
+$opt = mso_get_option('footer_any1_block', getinfo('template'), '');
+
+$CI = & get_instance();	
+
+$copy_maxsite = sprintf( tf('Работает на <a href="http://max-3000.com/">MaxSite CMS</a> | Время: {elapsed_time} | SQL: %s | Память: {memory_usage}'), $CI->db->query_count) . '<!--global_cache_footer--> | ';
+
+if (is_login())
+	$login = '<a href="' . getinfo('siteurl') . 'admin">' . tf('Управление') . '</a> | '
+		. '<a href="' . getinfo('siteurl') . 'logout">' . tf('Выйти') . '</a>';
+else
+	$login = '<a href="' . getinfo('siteurl') . 'login">' . tf('Вход') . '</a>';
+
+// используем php-шаблонизатор
+eval(mso_tmpl_prepare($opt));
+
+# end of file

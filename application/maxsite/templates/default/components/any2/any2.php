070d8f782afc1c60387a3fca062e8fdb26151742
MAX <maxsite.org@gmail.com>
2015-10-25 13:07:44 +0200

    Новый компонент any2 (произвольный блок)

---
+++ application/maxsite/templates/default/components/any2/any2.php
@@ -0,0 +1,20 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed'); 
+/*
+    (c) MaxSite CMS, http://max-3000.com/
+*/
+
+// условие вывода компонента
+// php-условие как в виджетах
+if ($rules = trim(mso_get_option('any2_rules_output', getinfo('template'), '')))
+{
+	$rules_result = eval('return ( ' . $rules . ' ) ? 1 : 0;');
+	if ($rules_result === false) $rules_result = 1;
+	if ($rules_result !== 1) return;
+}
+
+$opt = mso_get_option('any2_block', getinfo('template'), '');
+
+// используем php-шаблонизатор
+eval(mso_tmpl_prepare($opt));
+
+# end of file

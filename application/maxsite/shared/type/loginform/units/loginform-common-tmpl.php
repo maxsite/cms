93bf1c162b519dce3964f15c82b8577740f2b620
MAX <maxsite.org@gmail.com>
2015-11-29 12:33:46 +0200

    Дополнительные css-классы в форме логина

--- application/maxsite/shared/type/loginform/units/loginform-common-tmpl.php
+++ application/maxsite/shared/type/loginform/units/loginform-common-tmpl.php
@@ -4,11 +4,11 @@
 	<input type="hidden" value="{{ $redirect }}" name="flogin_redirect">
 	<input type="hidden" value="{{ $session_id }}" name="flogin_session_id">
 	
-	<p><label><span>{{ $login }}</span><input type="text" value="" name="flogin_user" {{ $login_add }}></label></p>
+	<p class="mso-loginform-login"><label><span>{{ $login }}</span><input type="text" value="" name="flogin_user" {{ $login_add }}></label></p>
 	
-	<p><label><span>{{ $password }}</span><input type="password" value="" name="flogin_password" {{ $password_add }}></label></p>
+	<p class="mso-loginform-password"><label><span>{{ $password }}</span><input type="password" value="" name="flogin_password" {{ $password_add }}></label></p>
 	
-	<p><span>{{ $submit }}</span><button type="submit" name="flogin_submit">{{ $submit_value }}</button>{{ $submit_end }}</p>
+	<p class="mso-loginform-submit"><span>{{ $submit }}</span><button type="submit" name="flogin_submit">{{ $submit_value }}</button>{{ $submit_end }}</p>
 	
 	{{ $hook_login_form_auth }}
 	{{ $form_end }}

9fa34f4474d134986d6a1f8adc12e7806404dbaf
Leonis <tv-games@mail.ru>
2015-12-06 09:22:08 +0200

    Unified SHJS plugin for other syntax highlight patterns

--- application/maxsite/plugins/shjs/info.php
+++ application/maxsite/plugins/shjs/info.php
@@ -3,7 +3,7 @@
 $info = array(
 	'name' => t('Подсветка кода'),
 	'description' => t('Подсветка кодов (JS, PHP, CSS, HTML, LESS). Подробное описание см. в опциях плагина.'),
-	'version' => '1.6',
+	'version' => '1.7',
 	'author' => 'Максим',
 	'plugin_url' => 'http://max-3000.com/',
 	'author_url' => 'http://shjs.sourceforge.net/',

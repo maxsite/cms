e194bcfa4891e50e3eed32bd80d1e337503c220a
MAX <maxsite.org@gmail.com>
2017-01-26 13:21:43 +0200

    Верстка users-форм приведена к общему варианту

--- application/maxsite/shared/type/users/units/users-loginform-tmpl.php
+++ application/maxsite/shared/type/users/units/users-loginform-tmpl.php
@@ -2,15 +2,15 @@
 
 <h3>{{ tf('Для редактирования введите свой email и пароль') }}</h3>
 
-<form method="post" class="mso-comusers-form">{{ mso_form_session('flogin_session_id') }}
+<div class="mso-comusers-form"><form method="post">{{ mso_form_session('flogin_session_id') }}
 
 	<input type="hidden" value="{{ $login_redirect }}" name="flogin_redirect">
 
-	<p><label>{{ tf('Ваш email') }} <input type="email" name="flogin_user"></label></p>
+	<p><label><span>{{ tf('Ваш email') }}</span> <input type="email" name="flogin_user"></label></p>
 
-	<p><label>{{ tf('Ваш пароль') }} <input type="password" name="flogin_password"></label></p>
+	<p><label><span>{{ tf('Ваш пароль') }}</span> <input type="password" name="flogin_password"></label></p>
 
 	<p><button type="submit" name="flogin_submit">{{ tf('Отправить') }}</button>
-	<a href="{{ $lost_link }}">{{ tf('Я забыл пароль') }}</a></p>
+	<a class="mso-i-forgot-password" href="{{ $lost_link }}">{{ tf('Я забыл пароль') }}</a></p>
 
-</form>
+</form></div>

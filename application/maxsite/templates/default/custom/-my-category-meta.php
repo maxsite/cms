e6bef5c07ee4552545d9735bd4337c910ed0ddc9
MAX <maxsite.org@gmail.com>
2017-08-25 10:51:43 +0300

    Пример файла для метаполей рубрик

---
+++ application/maxsite/templates/default/custom/-my-category-meta.php
@@ -0,0 +1,25 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed'); 
+
+/*
+ * (c) MaxSite CMS
+ * http://max-3000.com/
+ *
+ * Пример файла для метаполей рубрик. Для использования переименовать в my-category-meta.php
+ * Вывод метаполей реализуеся вебмастером отдельным кодом.
+ */
+
+return array(
+
+	'mytitle' => array(
+		'name' => 'Title в браузере', // имя поля
+		'attr' => 'rows=1',          // дополнительные атрибуты 
+		),
+		
+	'mydescription' => array(
+		'name' => 'Дополнительное описание',
+		'attr' => 'rows=2',
+		),
+
+);
+
+# end of file

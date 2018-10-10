aeb5d7971ea97d295497ee82ce52ab2170aeb4f6
MAX <maxsite.org@gmail.com>
2018-10-10 14:26:18 +0300

    Плагин sape

--- application/maxsite/plugins/sape/info.php
+++ application/maxsite/plugins/sape/info.php
@@ -3,7 +3,7 @@
 $info = array(
 	'name' => t('Ссылки Sape.ru'),
 	'description' => t('Плагин для работы с <a href="http://www.sape.ru/r.aa92aef9c6.php" target="_blank">sape.ru</a>'),
-	'version' => '1.5',
+	'version' => '1.6',
 	'author' => 'Максим',
 	'plugin_url' => '',
 	'author_url' => '//maxsite.org/',

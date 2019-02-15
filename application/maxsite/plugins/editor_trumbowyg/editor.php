5c9c6de1bc26f00980f9120b861a4300d41df49a
MAX <maxsite.org@gmail.com>
2019-02-15 19:27:33 +0200

    Новый плагин editor_trumbowyg

---
+++ application/maxsite/plugins/editor_trumbowyg/editor.php
@@ -0,0 +1,34 @@
+<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+// стили
+echo mso_load_style($editor_config['url'] . 'dist/ui/trumbowyg.min.css');
+
+
+// скрипты
+echo mso_load_script(getinfo('plugins_url') . 'tabs/tabs.js', false); // чтобы работали табы
+echo mso_load_script($editor_config['url'] . 'dist/trumbowyg.min.js');
+
+
+// автосохранение реализуется на уровне самого редактора, где нужно перехватить например Ctrl+S
+// необходимо передать в ajax-адрес и id-записи. 
+// Обработчик аякс запроса можно взять из admin\plugins\editor_markitup\autosave-post-ajax.php
+// в этом плагине автосохранения нет, но код оставлен (закомментированным) для примера
+
+// $auto_id = mso_segment(3); // номер страницы по сегменту url
+// if (!is_numeric($auto_id)) $auto_id = 0; // ошибочный id
+// js-код
+// autosaveurl = '< ?= getinfo('ajax') . base64_encode('plugins/editor_trumbowyg/autosave-post-ajax.php') ? >';
+// autosaveid = '< ?= $auto_id ? >';
+
+?>
+<script>
+$(document).ready(function(){
+	$('#f_content').trumbowyg();	
+});
+</script>
+<?= $editor_config['do_script'] ?>
+<form method="post" <?= $editor_config['action'] ?> enctype="multipart/form-data" id="form_editor">
+<?= $editor_config['do'] ?>
+<textarea id="f_content" name="f_content" rows="25" cols="80" style="height: <?= $editor_config['height'] ?>px;" ><?= $editor_config['content'] ?></textarea>
+<?= $editor_config['posle'] ?>
+</form>
---
+++ application/maxsite/plugins/editor_trumbowyg/index.php
@@ -0,0 +1,59 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed'); 
+/**
+ * MaxSite CMS
+ * (c) http://max-3000.com/
+ */
+
+function editor_trumbowyg_autoload($args = array())
+{	
+	mso_hook_add('editor_custom', 'editor_trumbowyg'); // хук на подключение своего редактора
+}
+
+function editor_trumbowyg($args = array()) 
+{
+	$options = mso_get_option('editor_options', 'admin', array() ); // получаем опции
+	
+	$editor_config['url'] = getinfo('plugins_url') . 'editor_trumbowyg/';
+	$editor_config['dir'] = getinfo('plugins_dir') . 'editor_trumbowyg/';
+
+	if (isset($args['content'])) $editor_config['content'] = $args['content'];
+	else $editor_config['content'] = '';
+		
+	if (isset($args['do'])) $editor_config['do'] = $args['do'];
+		else $editor_config['do'] = '';
+	
+	if (isset($args['do_script'])) $editor_config['do_script'] = $args['do_script'];
+		else $editor_config['do_script'] = '';
+		
+	if (isset($args['posle'])) $editor_config['posle'] = $args['posle'];
+		else $editor_config['posle'] = '';	
+		
+	if (isset($args['action'])) $editor_config['action'] = ' action="' . $args['action'] . '"';
+		else $editor_config['action'] = '';
+	
+	if (isset($args['height'])) $editor_config['height'] = (int) $args['height'];
+	else 
+	{
+		$editor_options = mso_get_option('editor_options', 'admin', array());
+		
+		if (isset($editor_options['editor_height']) and $editor_options['editor_height'] > 0)
+		{
+			$editor_config['height'] = (int) $editor_options['editor_height'];
+			if ($editor_config['height'] < 100) $editor_config['height'] = 400;
+		}
+		else
+		{
+			$editor_config['height'] = 400;
+		}
+	}
+
+	// Здесь можно произвести замены для правильного отображения контента
+
+	// Приведение строк с <br> в первозданный вид
+	// $editor_config['content'] = preg_replace('"&lt;br\s?/?&gt;"i', "\n", $editor_config['content']);
+	// $editor_config['content'] = preg_replace('"&lt;br&gt;"i', "\n", $editor_config['content']);
+
+	require($editor_config['dir'] . 'editor.php');
+}
+
+# end of file

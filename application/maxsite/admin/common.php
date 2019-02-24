985b24d1961ef34b4c6034b2e5e2d69c43491ab9
MAX <maxsite.org@gmail.com>
2019-02-24 10:07:27 +0200

    Fix по пагинации админки

--- application/maxsite/admin/common.php
+++ application/maxsite/admin/common.php
@@ -204,10 +204,9 @@ function mso_plugin_uninstall($f_name)
 */
 function mso_admin_pagination($r)
 {
-	
 	// используем плагин pagination
 	if (!function_exists('pagination_go'))
-		require_once(getinfo('plugins_dir') . 'plugins/pagination/index.php');
+		require_once(getinfo('plugins_dir') . 'pagination/index.php');
 	
 	// только со своими настройками, не зависимо от опций плагина 
 	$r['format'] = array();

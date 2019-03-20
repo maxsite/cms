0e4e127dd809e2588b9603c050ce61f694765a85
MAX <maxsite.org@gmail.com>
2019-03-20 09:17:22 +0200

    Фикс закрывающего тэга в sitemap #327

--- application/maxsite/shared/type/sitemap/sitemap.php
+++ application/maxsite/shared/type/sitemap/sitemap.php
@@ -9,7 +9,7 @@ if ($fn = mso_find_ts_file('main/main-start.php')) require($fn);
 
 	echo '<div class="mso-type-sitemap"><div class="mso-page-only">';
 	
-	echo '<h3>' . tf('Карта сайта (архив)') . '</h1>';
+	echo '<h3>' . tf('Карта сайта (архив)') . '</h3>';
 	echo '<div class="mso-page-content mso-type-sitemap-content">';
 	
 	if ($f = mso_page_foreach('sitemap')) 

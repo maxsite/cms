9b8dfa71258bd02bff59087dff6e511ff9ba104d
MAX <maxsite.org@gmail.com>
2019-04-22 14:39:34 +0300

    В bbcode добавил языковой перевод

--- application/maxsite/plugins/bbcode/info.php
+++ application/maxsite/plugins/bbcode/info.php
@@ -1,9 +1,9 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed'); 
 
 $info = array(
-	'name' => t('BB-коды'),
-	'description' => t('Конвертирует тэги BBCode в HTML'),
-	'version' => '8',
+	'name' => t('BB-коды', __FILE__),
+	'description' => t('Конвертирует тэги BBCode в HTML', __FILE__),
+	'version' => '9',
 	'author' => 'Максим',
 	'plugin_url' => '',
 	'author_url' => '//maxsite.org/',
---
+++ application/maxsite/plugins/bbcode/language/en.php
@@ -0,0 +1,26 @@
+<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Language for MaxSite CMS (c) http://max-3000.com/
+ * 
+ * Language: EN frontend
+ *
+ * Version MaxSite CMS: 103
+ * Author: MAX
+ * Author URL: http://maxsite.org/ 
+ */
+ 
+$lang = array(
+	'Где использовать' => 'Where to use',
+	'Укажите, где должен работать плагин' => 'Specify where the plugin should work',
+	'1||На страницах #2||В комментариях #3||На страницах и в комментариях' => '1||On pages # 2||In comments # 3||On pages and in comments',
+	'Использовать устаревшие bb-коды' => 'Use obsolete bb-codes',
+	'Вместо старого варианта, используйте новый:' => 'Instead of the old version, use the new one:',
+	'Настройки плагина bbcode' => 'Plugin settings bb-code',
+	'Укажите необходимые опции.' => 'Specify the required options.',
+	'Показать/Скрыть...' => 'Show / Hide ...',
+	'Конвертирует тэги BBCode в HTML' => 'Converts BB-Code tags to HTML',
+	'BB-коды' => 'BB-Code',
+);
+
+# end of file

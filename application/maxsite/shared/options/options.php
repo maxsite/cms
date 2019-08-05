5ee894bac2b00a0929c1b7524ccd32940b35829e
MAX <maxsite.org@gmail.com>
2019-08-05 11:44:05 +0300

    В компонентах основной файл может быть index.php

--- application/maxsite/shared/options/options.php
+++ application/maxsite/shared/options/options.php
@@ -1,4 +1,4 @@
-<?php if (!defined('BASEPATH')) exit('No direct script access allowed'); 
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 /**
  * MaxSite CMS
@@ -9,11 +9,11 @@
 # Сами опции вынесены в ini-файлы каталога options шаблона
 # поддерживаются custom/my_options.ini и custom/my_options.php
 
-echo '<h1>' . t('Настройка шаблона') . ' «'. getinfo('template_name') . '»</h1>';
+echo '<h1>' . t('Настройка шаблона') . ' «' . getinfo('template_name') . '»</h1>';
 // echo '<p class="info">' . t('Выберите необходимые опции') . '</p>';
 
 // функции для работы с ini-файлом
-require_once( getinfo('common_dir') . 'inifile.php' );
+require_once(getinfo('common_dir') . 'inifile.php');
 
 // проверка на обновление POST
 if (mso_check_post_ini()) echo '<div class="update">' . t('Обновлено!') . '</div>';
@@ -22,36 +22,30 @@ if (mso_check_post_ini()) echo '<div class="update">' . t('Обновлено!')
 $options = array();
 
 // если переменная = true, то подгружаем дефолтные опции из shared/options/default/ 
-if (mso_get_val('get_options_default', true))
-{
+if (mso_get_val('get_options_default', true)) {
 	// получим список всех файлов из options
 	$files = mso_get_path_files(getinfo('shared_dir') . 'options/default/', getinfo('shared_dir') . 'options/default/', true, array('ini'));
 
-	foreach($files as $file)
-	{
+	foreach ($files as $file) {
 		$add = mso_get_ini_file($file);
 		$options = array_merge($options, $add);
 	}
-	
 }
 
 // получим список всех файлов из options шаблона
 $files = mso_get_path_files(getinfo('template_dir') . 'options/', getinfo('template_dir') . 'options/', true, array('ini'));
 
-foreach($files as $file)
-{
+foreach ($files as $file) {
 	$add = mso_get_ini_file($file);
 	$options = array_merge($options, $add);
 }
 
-if (file_exists(getinfo('template_dir') . 'custom/my_options.php')) 
-{
+if (file_exists(getinfo('template_dir') . 'custom/my_options.php')) {
 	require(getinfo('template_dir') . 'custom/my_options.php');
 }
 
-if (file_exists(getinfo('template_dir') . 'custom/my_options.ini'))
-{
-	$add = mso_get_ini_file( getinfo('template_dir') . 'custom/my_options.ini'); // и свой
+if (file_exists(getinfo('template_dir') . 'custom/my_options.ini')) {
+	$add = mso_get_ini_file(getinfo('template_dir') . 'custom/my_options.ini'); // и свой
 	$options = array_merge($options, $add);
 }
 
@@ -61,33 +55,29 @@ if (file_exists(getinfo('template_dir') . 'custom/my_options.ini'))
 // подключаем только те опции и ini компонентов, которые реально существуют
 
 // каждый компонент в своем каталоге
-$all_component =  mso_get_dirs(getinfo('template_dir') . 'components/', array(), true, false);
+$all_component =  mso_get_dirs(getinfo('template_dir') . 'components/', array(), false, false);
 
 // проверяем опции (options.php)
-foreach($all_component as $dir) 
-{
+foreach ($all_component as $dir) {
 	$file = getinfo('template_dir') . 'components/' . $dir . '/options.php';
 	if (file_exists($file)) require($file); // php-файлы
 }
 
 // проверяем options.ini
-foreach($all_component as $dir) 
-{
+foreach ($all_component as $dir) {
 	$file = getinfo('template_dir') . 'components/' . $dir . '/options.ini';
-	
-	if (file_exists($file))
-	{
+
+	if (file_exists($file)) {
 		$add = mso_get_ini_file($file);
-		
+
 		// чтобы позволить испльзовать в компонентах одинаковые названия опций,
 		// добавим к названию имя компонета в скобках
 		$a1 = array();
-		
-		foreach($add as $key => $val)
-		{
-			$a1[$key . ' (' . $dir . ')'] = $val; 
+
+		foreach ($add as $key => $val) {
+			$a1[$key . ' (' . $dir . ')'] = $val;
 		}
-		
+
 		$options = array_merge($options, $a1);
 	}
 }
--- application/maxsite/templates/default/custom/template.php
+++ application/maxsite/templates/default/custom/template.php
@@ -1,4 +1,4 @@
-<?php if (!defined('BASEPATH')) exit('No direct script access allowed'); 
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 /*
  * (c) MaxSite CMS
@@ -16,14 +16,14 @@
 function my_default_head_section()
 {
 	global $page;
-	
+
 	// атирибуты <HTML>
 	$html_attr = mso_get_val('head_section_html_add');
 	$html_attr = mso_hook('html_attr', $html_attr);
 	$html_attr = $html_attr ? ' ' . $html_attr : '';
-	
-	echo 
-'<!DOCTYPE HTML>
+
+	echo
+		'<!DOCTYPE HTML>
 <html' . $html_attr . '><head>' . mso_hook('head_start') . '
 <meta charset="UTF-8">
 <title>' . mso_head_meta('title') . '</title>
@@ -32,82 +32,73 @@ function my_default_head_section()
 <meta name="description" content="' . mso_head_meta('description') . '">
 <link rel="shortcut icon" href="' . getinfo('uploads_url') . 'favicons/' . mso_get_option('default_favicon', 'templates', 'favicon1.png') . '" type="image/x-icon">
 ';
-	
+
 	if (mso_get_option('default_canonical', 'templates', 0)) echo mso_link_rel('canonical');
 
 	echo mso_rss();
-	
+
 	if ($fn = mso_fe('custom/head-start.php')) require($fn);
 
 	// autoload файлов
-	if ($autoload_css = mso_get_path_files(getinfo('template_dir') . 'assets/css/', getinfo('template_url') . 'assets/css/', true, array('css')))
-	{
+	if ($autoload_css = mso_get_path_files(getinfo('template_dir') . 'assets/css/', getinfo('template_url') . 'assets/css/', true, array('css'))) {
 		// поднимаем style.css самым первым
 		$fn_style_css = getinfo('template_url') . 'assets/css/style.css';
-		
-		if ($key = array_search($fn_style_css, $autoload_css))
-		{
-			if ($key !== false)
-			{
+
+		if ($key = array_search($fn_style_css, $autoload_css)) {
+			if ($key !== false) {
 				unset($autoload_css[$key]);
 				array_unshift($autoload_css, $fn_style_css);
 			}
 		}
-		
-		foreach($autoload_css as $fn_css)
-		{
+
+		foreach ($autoload_css as $fn_css) {
 			echo mso_load_style($fn_css);
 		}
 	}
-	
+
 	my_out_component_css();
 	mso_hook('head_css');
 	my_default_out_profiles();
-	
+
 	// своя версия jQuery, если нужно
-	if ($j = mso_get_val('jquery_url', false))
-	{
+	if ($j = mso_get_val('jquery_url', false)) {
 		echo mso_load_script($j);
-	}
-	else
-	{
-		if (mso_fe('assets/js/jquery.min.js')) 
+	} else {
+		if (mso_fe('assets/js/jquery.min.js'))
 			mso_add_file('assets/js/jquery.min.js');
-		else 
+		else
 			echo mso_load_jquery();
 	}
-	
+
 	mso_hook('head');
 
 	// autoload js-файлов
-	if ($autoload_js = mso_get_path_files(getinfo('template_dir') . 'assets/js/autoload/', getinfo('template_url') . 'assets/js/autoload/', true, array('js')))
-	{
-		foreach($autoload_js as $fn_js)
-		{
+	if ($autoload_js = mso_get_path_files(getinfo('template_dir') . 'assets/js/autoload/', getinfo('template_url') . 'assets/js/autoload/', true, array('js'))) {
+		foreach ($autoload_js as $fn_js) {
 			echo '<script src="' . $fn_js . '"></script>' . NR;
 		}
 	}
-	
+
 	my_out_component_js();
-	
+
 	if ($fn = mso_fe('custom/head.php')) require($fn);
 	if ($fn = mso_page_foreach('head')) require($fn);
 	if (function_exists('ushka')) echo ushka('head');
-	
-	if (mso_fe('assets/js/my.js')) 
+
+	if (mso_fe('assets/js/my.js'))
 		mso_add_file('assets/js/my.js');
-	
-	if ($my_style = mso_get_option('my_style', getinfo('template'), '')) 
+
+	if ($my_style = mso_get_option('my_style', getinfo('template'), ''))
 		echo NR . '<!-- custom css-my_style -->' . NR . '<style>' . NR . $my_style . '</style>';
-	
+
 	mso_hook('head_end');
 
 	if (function_exists('ushka')) echo ushka('google_analytics_top');
-	
+
 	echo NR . '</head>';
-} 
- 
- 
+}
+
+
 /**
  * вывод подключенных css-профилей
  * @global type $page
@@ -116,48 +107,42 @@ function my_default_head_section()
 function my_default_out_profiles($path = 'assets/css/profiles/')
 {
 	global $page;
-	
-	if ($default_profiles = mso_get_option('default_profiles', getinfo('template'), array())) // есть какие-то профили оформления
-	{
+
+	if ($default_profiles = mso_get_option('default_profiles', getinfo('template'), array())) {
+		// есть какие-то профили оформления
 		$css_out = '';
-		
+
 		// theme-профили подключаются как link rel="stylesheet
-		foreach($default_profiles as $css_file)
-		{
+		foreach ($default_profiles as $css_file) {
 			$fn = $path . $css_file;
-			
+
 			$link = strpos($css_file, 'theme-'); // это theme- ?
-			
+
 			if ($link !== false and $link === 0)
 				mso_add_file($fn); // подключаем внешими стилями
 			else
 				// получение и обработка CSS из файла
-				$css_out .= my_out_css_file($fn, false, false); 
+				$css_out .= my_out_css_file($fn, false, false);
 		}
-		
-		if ($css_out) 
+
+		if ($css_out)
 			echo '<style>' . $css_out . '</style>' . NR;
 	}
-	
+
 	// здесь же выводим css-профиль записи
 	// он задан через метаполе
-	
-	if (is_type('page') and isset($page) and $page)
-	{
-		if ($page_css_profiles = mso_page_meta_value('page_css_profiles', $page['page_meta']))
-		{
+
+	if (is_type('page') and isset($page) and $page) {
+		if ($page_css_profiles = mso_page_meta_value('page_css_profiles', $page['page_meta'])) {
 			$fn = $path . $page_css_profiles;
-		
+
 			$link = strpos($page_css_profiles, 'theme-'); // это theme- ?
-			
-			if ($link !== false and $link === 0)
-			{
+
+			if ($link !== false and $link === 0) {
 				mso_add_file($fn); // подключаем внешими стилями
-			}
-			else
-			{
+			} else {
 				// получение и обработка CSS из файла
-				if ($css_out = my_out_css_file($fn, false, false)) 
+				if ($css_out = my_out_css_file($fn, false, false))
 					echo NR . '<style>' . $css_out . '</style>' . NR;
 			}
 		}
@@ -175,14 +160,17 @@ function my_default_out_profiles($path = 'assets/css/profiles/')
  */
 function my_get_component_fn($option = '', $def_component = '')
 {
-	if ($dir = mso_get_option($option, getinfo('template'), $def_component)) // получение опции
-	{
+	// получение опции
+	if ($dir = mso_get_option($option, getinfo('template'), $def_component)) {
 		$fn = getinfo('template_dir') . 'components/' . $dir . '/' . $dir . '.php';
-		
-		// проверяем если файл в наличии
+
 		if (file_exists($fn)) return $fn;
+		else {
+			$fn = getinfo('template_dir') . 'components/' . $dir . '/index.php';
+			if (file_exists($fn)) return $fn;
+		}
 	}
-	
+
 	return false; // ничего нет
 }
 
@@ -197,11 +185,9 @@ function my_get_component_fn($option = '', $def_component = '')
 function my_out_component_css($component_options = array('header_component1', 'header_component2', 'header_component3', 'header_component4', 'header_component5', 'footer_component1', 'footer_component2', 'footer_component3', 'footer_component4', 'footer_component5'))
 {
 	// проходимся по всем заданным опциям
-	foreach($component_options as $option)
-	{
+	foreach ($component_options as $option) {
 		// и если они определены
-		if ($dir = mso_get_option($option, getinfo('template'), false))
-		{
+		if ($dir = mso_get_option($option, getinfo('template'), false)) {
 			$fn = 'components/' . $dir . '/style.css';
 			mso_add_file($fn); // подключаем внешими стилями
 		}
@@ -217,11 +203,9 @@ function my_out_component_css($component_options = array('header_component1', 'h
 function my_out_component_js($component_options = array('header_component1', 'header_component2', 'header_component3', 'header_component4', 'header_component5', 'footer_component1', 'footer_component2', 'footer_component3', 'footer_component4', 'footer_component5'))
 {
 	// проходимся по всем заданным опциям
-	foreach($component_options as $option)
-	{
+	foreach ($component_options as $option) {
 		// и если они определены
-		if ($dir = mso_get_option($option, getinfo('template'), false))
-		{
+		if ($dir = mso_get_option($option, getinfo('template'), false)) {
 			$fn = 'components/' . $dir . '/' . $dir . '.js';
 			mso_add_file($fn);
 		}
@@ -245,48 +229,46 @@ function my_out_component_js($component_options = array('header_component1', 'he
  */
 function my_out_css_file($fn, $tag_style = true, $echo = true)
 {
-	
 	$fn = getinfo('template_dir') . $fn;
-	
+
 	$out = '';
 	if (file_exists($fn)) // проверяем если ли файл в наличии
 	{
 		if ($r = @file_get_contents($fn)) $out .= $r . NR; // получаем содержимое
-	
-		if ($out) 
-		{
+
+		if ($out) {
 			ob_start();
-			eval( '?>' . stripslashes($out) . '<?php ');
+			eval('?>' . stripslashes($out) . '<?php ');
 			$out = ob_get_contents();
 			ob_end_clean();
-			
+
 			$out = str_replace('[TEMPLATE_URL]', getinfo('template_url'), $out);
 			$out = str_replace(array("\r\n", "\r", "\n", "\t", '  ', '    ', '    '), '', $out);
 			$out = str_replace(array('; ', ' {', ': ', ', '), array(';', '{', ':', ','), $out);
 		}
-	
+
 		if ($tag_style) $out = NR . '<style>' . $out . '</style>' . NR;
 		if ($echo) echo $out;
 	}
-	
+
 	return $out;
 }
 
 
 /**
-*  сжатие HTML-текста путём удаления лишних пробелов
-*  функция из LPF http://lpf.maxsite.com.ua/
-* 
-*  @param $text входной текст
-*  @return string
-*/
+ *  сжатие HTML-текста путём удаления лишних пробелов
+ *  функция из LPF http://lpf.maxsite.com.ua/
+ * 
+ *  @param $text входной текст
+ *  @return string
+ */
 function my_compress_text($text)
 {
 	# защищенный текст
 	$text = preg_replace_callback('!(<pre.*?>)(.*?)(</pre>)!is', 'my_clean_pre_do', $text);
 	$text = preg_replace_callback('!(<code.*?>)(.*?)(</code>)!is', 'my_clean_pre_do', $text);
 	$text = preg_replace_callback('!(<script.*?>)(.*?)(</script>)!is', 'my_clean_html_script', $text);
-	
+
 	$text = str_replace(array("\r\n", "\r"), "\n", $text);
 	$text = str_replace("\t", ' ', $text);
 	$text = str_replace("\n   ", "\n", $text);
@@ -295,24 +277,24 @@ function my_compress_text($text)
 	$text = str_replace("\n", '', $text);
 	$text = str_replace('   ', ' ', $text);
 	$text = str_replace('  ', ' ', $text);
-	
+
 	// специфичные замены
 	$text = str_replace('<!---->', '', $text);
 	$text = str_replace('>    <', '><', $text);
 	$text = str_replace('>   <', '><', $text);
 	$text = str_replace('>  <', '><', $text);
-	
+
 	$text = preg_replace_callback('!\[html_base64\](.*?)\[\/html_base64\]!is', 'my_clean_html_posle', $text);
-	
+
 	return $text;
 }
 
 /**
-*  pre, которое загоняется в [html_base64]
-*  
-*  @param $matches matches
-*  @return string
-*/
+ *  pre, которое загоняется в [html_base64]
+ *  
+ *  @param $matches matches
+ *  @return string
+ */
 function my_clean_pre_do($matches)
 {
 	$text = trim($matches[2]);
@@ -325,38 +307,38 @@ function my_clean_pre_do($matches)
 	$text = str_replace("<br />", "<br>", $text);
 	$text = str_replace("<br/>", "<br>", $text);
 	$text = str_replace("<br>", "\n", $text);
-	
+
 	$text = str_replace('<', '&lt;', $text);
 	$text = str_replace('>', '&gt;', $text);
 	$text = str_replace('&lt;pre', '<pre', $text);
 	$text = str_replace('&lt;/pre', '</pre', $text);
 	$text = str_replace('pre&gt;', 'pre>', $text);
 
-	$text = $matches[1] . "\n" . '[html_base64]' . base64_encode($text) . '[/html_base64]'. $matches[3];
+	$text = $matches[1] . "\n" . '[html_base64]' . base64_encode($text) . '[/html_base64]' . $matches[3];
 
 	return $text;
 }
 
 /**
-*  script, который загоняется в [html_base64]
-*  
-*  @param $matches matches
-*  @return string
-*/
+ *  script, который загоняется в [html_base64]
+ *  
+ *  @param $matches matches
+ *  @return string
+ */
 function my_clean_html_script($matches)
 {
 	$text = trim($matches[2]);
-	$text = $matches[1] . '[html_base64]' . base64_encode($text) . '[/html_base64]'. $matches[3];
-	
+	$text = $matches[1] . '[html_base64]' . base64_encode($text) . '[/html_base64]' . $matches[3];
+
 	return $text;
 }
 
 /**
-*  декодирование из [html_base64]
-*  
-*  @param $matches matches
-*  @return string
-*/
+ *  декодирование из [html_base64]
+ *  
+ *  @param $matches matches
+ *  @return string
+ */
 function my_clean_html_posle($matches)
 {
 	return base64_decode($matches[1]);
@@ -364,11 +346,11 @@ function my_clean_html_posle($matches)
 
 
 /**
-*  заменяет в тексте все вхождения http:// и https:// на // 
-*  
-*  @param $text текст
-*  @return string
-*/
+ *  заменяет в тексте все вхождения http:// и https:// на // 
+ *  
+ *  @param $text текст
+ *  @return string
+ */
 function my_remove_protocol($text)
 {
 	# защищенный текст
@@ -376,12 +358,12 @@ function my_remove_protocol($text)
 	$text = preg_replace_callback('!(<code.*?>)(.*?)(</code>)!is', 'my_clean_pre_do', $text);
 	$text = preg_replace_callback('!(<script.*?>)(.*?)(</script>)!is', 'my_clean_html_script', $text);
 	$text = preg_replace_callback('!(<textarea.*?>)(.*?)(</textarea>)!is', 'my_clean_html_script', $text);
-	
+
 	$text = str_replace('https://', '//', $text);
 	$text = str_replace('http://', '//', $text);
-	
+
 	$text = preg_replace_callback('!\[html_base64\](.*?)\[\/html_base64\]!is', 'my_clean_html_posle', $text);
-		
+
 	return $text;
 }
 

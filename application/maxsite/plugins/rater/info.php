2acb6bcd85cf2950fce3b9d467f1b392033ba1a7
MAX <maxsite.org@gmail.com>
2021-02-07 09:06:26 +0200

    Небольшие правки по плагину rater

--- application/maxsite/plugins/rater/info.php
+++ application/maxsite/plugins/rater/info.php
@@ -3,7 +3,7 @@
 $info = array(
 	'name' => t('Рейтинг записей'),
 	'description' => t('Оценка статей посетителями'),
-	'version' => '2.2',
+	'version' => '2.3',
 	'author' => 'Максим',
 	'plugin_url' => '',
 	'author_url' => 'https://maxsite.org/',

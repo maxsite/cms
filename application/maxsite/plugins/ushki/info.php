65234cc60a71508f5e76a8caef1eb0eb4ebf1178
MAX <maxsite.org@gmail.com>
2021-04-29 10:41:23 +0300

    Плагин ушки новая функция

--- application/maxsite/plugins/ushki/info.php
+++ application/maxsite/plugins/ushki/info.php
@@ -3,10 +3,10 @@
 $info = array(
 	'name' => t('Ушки'),
 	'description' => t('Ушки позволяют размещать произвольный код/текст на сайте.'),
-	'version' => '2',
+	'version' => '2.1',
 	'author' => 'Максим',
 	'plugin_url' => '',
-	'author_url' => '//maxsite.org/',
+	'author_url' => 'https://maxsite.org/',
 	'group' => 'widget, text',
 	'options_url' => getinfo('site_admin_url') . 'plugin_ushki',
 );

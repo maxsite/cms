b9e2eeda251d4316d552909c26b7a9df8fed6d66
MAX <maxsite.org@gmail.com>
2021-04-29 10:43:12 +0300

    Опции шаблона ini-файлы могут быть в custom/options/

--- application/maxsite/shared/options/options.php
+++ application/maxsite/shared/options/options.php
@@ -21,30 +21,42 @@ $options = [];
 
 // если переменная = true, то подгружаем дефолтные ini-опции из shared/options/default/ 
 if (mso_get_val('get_options_default', true)) {
-	// получим список всех файлов из options
-	$files = mso_get_path_files(getinfo('shared_dir') . 'options/default/', getinfo('shared_dir') . 'options/default/', true, ['ini']);
+    // получим список всех файлов из options
+    $files = mso_get_path_files(getinfo('shared_dir') . 'options/default/', getinfo('shared_dir') . 'options/default/', true, ['ini']);
 
-	foreach ($files as $file) {
-		$add = mso_get_ini_file($file);
-		$options = array_merge($options, $add);
-	}
+    foreach ($files as $file) {
+        $add = mso_get_ini_file($file);
+        $options = array_merge($options, $add);
+    }
 }
 
 // получим список всех ini-файлов из options шаблона
 $files = mso_get_path_files(getinfo('template_dir') . 'options/', getinfo('template_dir') . 'options/', true, ['ini']);
 
 foreach ($files as $file) {
-	$add = mso_get_ini_file($file);
-	$options = array_merge($options, $add);
+    $add = mso_get_ini_file($file);
+    $options = array_merge($options, $add);
 }
 
 if (file_exists(getinfo('template_dir') . 'custom/my_options.php')) {
-	require getinfo('template_dir') . 'custom/my_options.php';
+    require getinfo('template_dir') . 'custom/my_options.php';
 }
 
 if (file_exists(getinfo('template_dir') . 'custom/my_options.ini')) {
-	$add = mso_get_ini_file(getinfo('template_dir') . 'custom/my_options.ini'); // и свой
-	$options = array_merge($options, $add);
+    $add = mso_get_ini_file(getinfo('template_dir') . 'custom/my_options.ini'); // и свой
+    $options = array_merge($options, $add);
+}
+
+// ini-файлы могут быть в custom/options/
+$opt_files = glob(getinfo('template_dir') . 'custom/options/*.ini');
+
+if ($opt_files) {
+    natsort($opt_files);
+
+    foreach ($opt_files as $f) {
+        $add = mso_get_ini_file($f);
+        $options = array_merge($options, $add);
+    }
 }
 
 // подключим все опции компонентов из components
@@ -57,29 +69,29 @@ $all_component =  mso_get_dirs(getinfo('template_dir') . 'components/', [], fals
 
 // проверяем опции компонентов (options.php)
 foreach ($all_component as $dir) {
-	$file = getinfo('template_dir') . 'components/' . $dir . '/options.php';
+    $file = getinfo('template_dir') . 'components/' . $dir . '/options.php';
 
-	// php-файл опций. в нём доступна $options, к которой добавляются свои опции
-	if (file_exists($file)) require $file;
+    // php-файл опций. в нём доступна $options, к которой добавляются свои опции
+    if (file_exists($file)) require $file;
 }
 
 // проверяем options.ini
 foreach ($all_component as $dir) {
-	$file = getinfo('template_dir') . 'components/' . $dir . '/options.ini';
+    $file = getinfo('template_dir') . 'components/' . $dir . '/options.ini';
 
-	if (file_exists($file)) {
-		$add = mso_get_ini_file($file);
+    if (file_exists($file)) {
+        $add = mso_get_ini_file($file);
 
-		// чтобы позволить использовать в компонентах одинаковые названия опций,
-		// добавим к названию имя компонета в скобках
-		$a1 = [];
+        // чтобы позволить использовать в компонентах одинаковые названия опций,
+        // добавим к названию имя компонета в скобках
+        $a1 = [];
 
-		foreach ($add as $key => $val) {
-			$a1[$key . ' (' . $dir . ')'] = $val;
-		}
+        foreach ($add as $key => $val) {
+            $a1[$key . ' (' . $dir . ')'] = $val;
+        }
 
-		$options = array_merge($options, $a1);
-	}
+        $options = array_merge($options, $a1);
+    }
 }
 
 // вывод всех ini-опций

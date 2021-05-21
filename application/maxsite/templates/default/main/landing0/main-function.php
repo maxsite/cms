eaf03bbfb420a3bb807d0e5ffcf5db3690e46cd7
MAX <maxsite.org@gmail.com>
2021-05-21 11:22:36 +0300

    Мелкие правки в main-файлах landing

--- application/maxsite/templates/default/main/landing0/main-function.php
+++ application/maxsite/templates/default/main/landing0/main-function.php
@@ -2,7 +2,7 @@
 
 mso_set_val('show_thumb', false);
 mso_set_val('body_class', mso_get_val('main_class', ''));
-mso_set_val('page_content_only', true);
+# mso_set_val('page_content_only', true);
 
 mso_remove_hook('content_end');
 mso_remove_hook('head_css');

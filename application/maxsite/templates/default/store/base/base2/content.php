d4950da12fc88d3cb20e68571570d1e22bca369e
MAX <maxsite.org@gmail.com>
2021-12-01 12:26:40 +0200

    Новый модуль base2

---
+++ application/maxsite/templates/default/store/base/base2/content.php
@@ -0,0 +1,13 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+<div class="layout-center-wrap"><div class="layout-wrap">
+	<div class="flex flex-wrap-tablet">
+        <div class="w48 w100-tablet bg-gray100 pad20">
+            COLUMN
+        </div>
+        
+        <div class="w48 w100-tablet bg-gray100 pad20">
+            COLUMN
+        </div>
+    </div>
+</div></div>
---
+++ application/maxsite/templates/default/store/base/base2/index.php
@@ -0,0 +1,7 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+[unit]
+module = base/base2
+require = content.php
+parser = autotag_simple
+[/unit]

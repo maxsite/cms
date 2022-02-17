5534ef093670ad8d02b9927ce77cc767aba5ea9d
MAX <maxsite.org@gmail.com>
2022-02-17 17:55:22 +0200

    MaxSite CMS 108.6

--- application/libraries/maxsite_lib.php
+++ application/libraries/maxsite_lib.php
@@ -12,7 +12,7 @@ require_once $MSO->config['common_dir'] . 'common.php';
 
 class Maxsite_lib
 {
-    public $version = '108.5';
+    public $version = '108.6';
     public $config = [];
     public $data = [];
     public $hooks = [];

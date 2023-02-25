113b23327fdfba8969b9c8b2cbe100760628889f
MAX <maxsite.org@gmail.com>
2023-02-25 16:17:57 +0200

    Plugin shjs

--- application/maxsite/plugins/shjs/info.php
+++ application/maxsite/plugins/shjs/info.php
@@ -3,10 +3,10 @@
 $info = array(
 	'name' => t('Подсветка кода'),
 	'description' => t('Подсветка кодов (JS, PHP, CSS, HTML, LESS). Подробное описание см. в опциях плагина.'),
-	'version' => '3.3',
+	'version' => '3.4',
 	'author' => 'Максим',
 	'plugin_url' => '',
-	'author_url' => 'http://shjs.sourceforge.net/',
+	'author_url' => 'https://shjs.sourceforge.net/',
 	'group' => 'text'
 );
 
